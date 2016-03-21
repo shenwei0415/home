fcc70b72ab91098d7ec3235753e27568e48ddec2
shenwei0415 <shenwei_1232126.com>
2016-03-21 16:53:17 +0800

    add some commits

--- boost_study/boost_bind.cpp
+++ boost_study/boost_bind.cpp
@@ -20,10 +20,11 @@ int main(int argc, char** atgv)
   v.push_back(0);
   v.push_back(-1);
   v.push_back(3);
-
+  //for_each需要的是一个参数的函数
   std::for_each(v.begin(), v.end(), boost::bind(print, _1));
   std::cout << std::endl;
 
+  //sort需要的是两个参数的函数
   std::sort(v.begin(), v.end(), boost::bind(compare, _1, _2));
   std::for_each(v.begin(),v.end(),boost::bind(print,_1));
   
